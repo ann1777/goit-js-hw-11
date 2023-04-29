c0c006b78f476595b5c72c40b804ca234debe5ff
Angela Potapchuk <angela.harkov@gmail.com>
2023-04-29 21:58:40 +0300

    Fixed fetchImages.js

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -6,15 +6,12 @@ async function fetchImages(name, page, perPage) {
   const baseURL = 'https://pixabay.com/api/';
   const key = '35889696-234245940e7cec3ffc17b1751';
 
-  try {
-    const response = await axios.get(
+  const response = await axios.get(
       `${baseURL}?key=${key}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
     );
     // console.log(response.data);
-    return response.data;
-  } catch (error) {
-    console.log('ERROR: ' + error);
-  }
+    const newImages = await response.data;
+    return newImages;
 }
 
 export { fetchImages };
