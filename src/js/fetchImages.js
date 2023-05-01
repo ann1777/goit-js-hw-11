59171dcb39a77923f5a99ad6eb4f5abdca78966b
Angela Potapchuk <angela.harkov@gmail.com>
2023-05-01 18:54:28 +0300

    Created fetchesImg function into a ImgsFetcher class

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -2,16 +2,28 @@ import axios from 'axios';
 
 // Fetch images from Pixabay API using Axios
 
-async function fetchImages(name, page, perPage) {
-  const baseURL = 'https://pixabay.com/api/';
-  const key = '35889696-234245940e7cec3ffc17b1751';
+export default class ImgsFetcher {
+    #BASE_URL = 'https://pixabay.com/api/';
+    #KEY = '35889696-234245940e7cec3ffc17b1751';
+    constructor() {
+        this.query = '';
+        this.page = 1;
+        this.per_page = 40;
+        this.totalPage = 1;
+    }
+    async getRequest() {
+        const parametrs = {
+          key: this.#KEY,
+          q: this.query,
+          page: this.page,
+          per_page: this.per_page,
+        }
+    const response = await axios.get(this.#BASE_URL, { params: parametrs });
 
-  const response = await axios.get(
-      `${baseURL}?key=${key}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
-    );
-    // console.log(response.data);
-    const newImages = await response.data;
-    return newImages;
+    if (response.data.total === 0) {Notify.failure("Sorry, there are no images matching your search query. Please try again.");} 
+
+    this.totalPage = Math.ceil(response.data.total / this.per_page);  
+
+    return response.data.hits;
+    };
 }
-
-export { fetchImages };
