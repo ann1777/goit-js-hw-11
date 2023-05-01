18850ccf25cdfa97ea31fbb40b43fed24696862e
Angela Potapchuk <angela.harkov@gmail.com>
2023-05-01 20:50:04 +0300

    Fixed markupDuilder

--- src/js/markupBuilder.js
+++ src/js/markupBuilder.js
@@ -1,46 +1,47 @@
 export default function markupBuilder(array) {
     const markupStrings = array.map(el => {
     return `<div class="photo-card">
-  
-          <a class="gallery-item" href="${el.largeImageURL}">
-            <img
-              class="gallery__image"
-              src="${el.webformatURL}"
-              alt="${el.tags}"
-              loading="lazy"
-          /></a>
-  
-          <div class="info">
-            <div class="info__box">
-              <p class="info-item">
-                <b class="material-symbols-outlined">thumb_up</b>
-              </p>
-              <p class="info-counter">${el.likes.toLocaleString()}</p>
-            </div>
-  
-            <div class="info__box">
-              <p class="info-item">
-                <b class="material-symbols-outlined">visibility</b>
-              </p>
-              <p class="info-counter">${el.views.toLocaleString()}</p>
-            </div>
-  
-            <div class="info__box">
-              <p class="info-item">
-                <b class="material-symbols-outlined">forum</b>
-              </p>
-              <p class="info-counter">${el.comments.toLocaleString()}</p>
-            </div>
-  
-            <div class="info__box">
-              <p class="info-item">
-                <b class="material-symbols-outlined">download</b>
-              </p>
-              <p class="info-counter">${el.downloads.toLocaleString()}</p>
-            </div>
-  
-          </div>
-        </div>`
+    <img src="${el.previewURL
+    }" alt="${el.tags}" loading="lazy" />
+        <div class="gallery-item" href="${el.largeImageURL}>
+        <img
+            class="gallery__image"
+            src="${el.webformatURL}"
+            alt="${el.tags}"
+            loading="lazy"
+        /></div>
+
+        <div class="info">
+        <div class="info__box">
+            <p class="info-item">
+            <b class="material-symbols-outlined">thumb_up</b>
+            </p>
+            <p class="info-counter">${el.likes.toLocaleString()}</p>
+        </div>
+
+        <div class="info__box">
+            <p class="info-item">
+            <b class="material-symbols-outlined">visibility</b>
+            </p>
+            <p class="info-counter">${el.views.toLocaleString()}</p>
+        </div>
+
+        <div class="info__box">
+            <p class="info-item">
+            <b class="material-symbols-outlined">forum</b>
+            </p>
+            <p class="info-counter">${el.comments.toLocaleString()}</p>
+        </div>
+
+        <div class="info__box">
+            <p class="info-item">
+            <b class="material-symbols-outlined">download</b>
+            </p>
+            <p class="info-counter">${el.downloads.toLocaleString()}</p>
+        </div>
+
+        </div>
+    </div>`
     })
     return markupStrings.join('')
 
