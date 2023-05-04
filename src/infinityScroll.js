8badeb2a37e3fdc156ed0175a592b431eca0e7c2
Angela Potapchuk <angela.harkov@gmail.com>
2023-05-04 16:13:08 +0300

    Added infinityScroll in infinityScroll.js

--- src/infinityScroll.js
+++ src/infinityScroll.js
@@ -1,9 +1,9 @@
 'use strict'
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import {Notify} from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import ImgsFetcher from './js/fetchImages';
-import markupBuilder from './js/markupBuilder';
+import { markupBuilder } from './js/markupBuilder';
 import { renderGallery } from './js/renderGallery';
 
 const inputFldEl = document.querySelector('input[name="searchQuery"]');
@@ -14,7 +14,8 @@ const searchForm = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
 const imagesFetcher = new ImgsFetcher();
-const simpleLightbox = new SimpleLightbox('.gallery a');
+const lightbox = new SimpleLightbox('.gallery a');
+const target = document.querySelector('#js-item');
 
 let page = 0;
 clearMarkup();
@@ -30,15 +31,16 @@ const options = {
 }
 const callback = function(entry, observer) {
     if (entry[0].isIntersecting) {
-        onBtnLoadClick();
+      onPageDownLoadMore();
 }};
 const observer = new IntersectionObserver(callback, options);
 
 async function getRandomPhotos() {
-    try {
-      const response = await imagesFetcher.getRandomPhotos();
-      const data = response.data;
-      const result = renderGallery(data);
+  try {
+    const response = await imagesFetcher.getRandomPhotos();
+    const data = response.data;
+    renderGallery(data);
+    lightbox.refresh();
     }
     catch (err) {
       console.log(err);
@@ -46,101 +48,70 @@ async function getRandomPhotos() {
 }
 
 async function onFormSubmit(e) {
-    e.preventDefault();
-    clearMarkup();
-    imagesFetcher.query = e.target.elements.searchQuery.value;
-    const data = await imagesFetcher.getRequest();
-    const pageMarkup = markupBuilder(data);
-    renderGallery(pageMarkup);
-    showCloseBtn();
-    showLoadMoreBtn();
-    if (data.total_pages === 0) {
-      clearMarkup()
-      Notify.failure('Nothing was found for your request');
-      return;
-    } 
-    if (imagesFetcher.page < imagesFetcher.totalPage) {
-      imagesFetcher.page += 1;
-      showLoadMoreBtn();
-      observer.observe(target);         
+  e.preventDefault();
+  clearMarkup();
+  imagesFetcher.query = e.target.elements.searchQuery.value;
+  const data = await imagesFetcher.getRequest();
+
+  // console.log(data);
+  const pageMarkup = markupBuilder(data);
+  insertGallery(pageMarkup);
+
+  if (data.total_pages === 0) {
+    clearMarkup()
+    Notify.failure('Nothing was found for your request');
+    return;
+  } 
+  if (imagesFetcher.page < imagesFetcher.totalPage) {
+    imagesFetcher.page += 1;
+    lightbox.refresh();
+    observer.observe(target);         
   }
 }
 
-function renderGallery(pageMarkup) {
+function insertGallery(pageMarkup) {
     gallery.insertAdjacentHTML('beforeend', pageMarkup);
 }
 
-async function onLoadMoreBtnClick() {
-    const data = await imagesFetcher.getRequest();
-    const pageMarkup = markupBuilder(data);
-    renderGallery(pageMarkup);
-    loadMoreBtn.addEventListener ('click',
-    async () => {
-      // console.log(imagesFetcher.page);
-      if (imagesFetcher.page === imagesFetcher.totalPage) { 
-        Notify.info("We're sorry, but you've reached the end of search results.");
-        hideLoadMoreBtn();
-        showCloseBtn();
-        observer.unobserve(target);}
-        else if (imagesFetcher.page < imagesFetcher.totalPage) {
-          imagesFetcher.page += 1; 
-          renderGallery();
-          showLoadMoreBtn();
-          showCloseBtn();
-      } try {
-        const { height: cardHeight } = document.querySelector(".gallery").firstElementChild.getBoundingClientRect();
-  
-      window.scrollBy({
-        top: cardHeight * 2,
-        behavior: "smooth",
-      });
-     } catch (err) {
-        Notify.failure('RenderGallery method error');}     
-     });
-}
+async function onPageDownLoadMore() {
+  imagesFetcher.page += 1;
+  const data = await imagesFetcher.getRequest();
+  const pageMarkup = markupBuilder(data);
+  insertGallery(pageMarkup);
+    // console.log(imagesFetcher.page);
+  if (imagesFetcher.page === imagesFetcher.totalPage) { 
+    Notify.info("We're sorry, but you've reached the end of search results.");
+    observer.unobserve(target);}
+    else if (imagesFetcher.page < imagesFetcher.totalPage) {
+      /* imagesFetcher.page += 1;  */
+      lightbox.refresh();
+  } try {
+    const { height: cardHeight } = document.querySelector(".gallery").firstElementChild.getBoundingClientRect();
 
-function onCloseBtnClick() {
-    closeBtn.addEventListener ('click',
-    () => {
-      console.log(gallery);
-      clearMarkup();
-      new SimpleLightbox('.gallery a');
-      getRandomPhotos();
-      clearInputFld();
-      page = 1;
-    });
-    searchForm.addEventListener('submit', onFormSubmit);
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: "smooth",
+  });
+  } catch (err) {
+    Notify.failure('RenderGallery method error');}     
 };
 
-function showCloseBtn() {
-    closeBtn.style.display = 'block';
-    closeBtn.addEventListener('click', onCloseBtnClick); 
-}
-  
-function hideLoadMoreBtn() {
-    loadMoreBtn.style.display = 'none';
-    loadMoreBtn.removeEventListener('click', onLoadMoreBtnClick);
 
+function insertGallery(pageMarkup) {
+  gallery.insertAdjacentHTML('beforeend', pageMarkup);
+  lightbox.refresh();
 }
+
+function hideLoadMoreBtn() {
+  loadMoreBtn.style.display = 'none';
+};
 
 function hideCloseBtn() {
-    closeBtn.style.display = 'none';
-    closeBtn.removeEventListener('click', onCloseBtnClick);
-}
-
-function clearInputFld() {
-    inputFldEl.value = "";
-    searchForm.addEventListener('submit', onFormSubmit);
-}
+  closeBtn.style.display = 'none';
+};
 
 function clearMarkup() {
     ImgsFetcher.page = 1;
     gallery.innerHTML = '';
-    hideCloseBtn();
-    hideLoadMoreBtn();
     searchForm.addEventListener('submit', onFormSubmit);
 }
-    
-
-
-
